1ef84ed601b92269667c5bd9e00ce3e87e96d81c
Abishek <abishekmanusha@gmail.com>
2020-09-27 23:57:50 +0530

    Add few changes in layout,drawables and class

---
+++ app/src/main/java/com/example/pc_house/AddNewAddress.java
@@ -0,0 +1,14 @@
+package com.example.pc_house;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_address);
+    }
+}
